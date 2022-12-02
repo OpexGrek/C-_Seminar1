b7695b99b3930f20bc1faf72a8c16d6be3752673
Oleg <starwarser2@gmail.com>
2022-12-02 07:52:23 +0700

    add .gitignore

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Input B: ");
+int B = int.Parse(Console.ReadLine());
+
+if (A > B)
+{
+    Console.WriteLine("Max = " + A);
+    Console.WriteLine("Min = " + B);
+}
+    
+if(A < B)
+{
+    Console.WriteLine("Max = " + B);
+    Console.WriteLine("Min = " + A);
+}
+ 
+if(A == B)    
+{
+    Console.WriteLine("Equal numbers");
+}    
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Input A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Input B: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Input C: ");
+int C = int.Parse(Console.ReadLine());
+
+int count = 0;
+int max = 0;
+
+while (count < 3)
+{
+    if (A > max)
+    {
+        max = A;      
+    }
+
+    if (B > max)
+    {
+        max = B;
+    }
+    
+    if (C > max)
+    {
+        max = C;
+    }
+    count++;
+}
+Console.WriteLine("Max = " + max);
