0d0be9a634334b64f7756653f2cae4c68ea9da6d
Oleg <starwarser2@gmail.com>
2022-12-02 19:28:47 +0700

    Task8 project

---
+++ Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Input N: ");
+int N = int.Parse(Console.ReadLine());
+int N0 = 1;
+int A = Math.Abs(N % 2);
+
+if (N == 1)
+{
+    Console.Write("There is no even numbers");
+}
+if (N < 1)
+{
+    Console.Write("You can input only integer");
+}
+while (N0 < N)
+{
+    if (A == 0)
+    {
+        Console.Write(N0 + 1 + " ");
+    }
+    else
+    {
+        Console.Write(N0 + 1 + " ");
+        if (N0 == N)
+        {
+            Console.Write(N0 + " ");
+        }
+    }
+    N0 = N0 + 2;
+}
