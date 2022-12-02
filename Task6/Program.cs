7ddbf652d0500fb8613522f928e6401773a8da88
Oleg <starwarser2@gmail.com>
2022-12-02 08:36:24 +0700

    Task6 project

---
+++ Task6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Input A: ");
+int A = int.Parse(Console.ReadLine());
+
+int B = Math.Abs(A % 2); // Превращаем отрицательный остаток в положительный
+
+if (B == 1)
+{
+    Console.WriteLine("Number " + A + " is odd");
+}
+else
+{
+    Console.WriteLine("Number " + A + " is even");
+}
